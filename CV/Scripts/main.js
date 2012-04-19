2bef18a8599c1f1d8c405835bf138a4a6760f1eb
Artem <artemdude@gmail.com>
2012-04-19 21:26:47 +0300

    Finish...?

---
+++ CV/Scripts/main.js
@@ -0,0 +1,42 @@
+﻿$(function () {
+
+    // create a style switch button
+    $('#ChangeCloudAppearance').toggle(
+		        function () {
+		            $("#tags ul").hide().addClass("alt").fadeIn("fast");
+		        },
+		        function () {
+		            $("#tags ul").hide().removeClass("alt").fadeIn("fast");
+		        }
+	        );
+
+
+    // create a sort by alphabet button
+    $('#sortCloudByAlphabet').toggle(
+		        function () {
+		            $("#tags ul li").tsort({ order: "asc" });
+		            $(this).addClass('filter-selected');
+		            $('#SortCloudByStrength').removeClass('filter-selected');
+		        },
+		        function () {
+		            $("#tags ul li").tsort({ order: "desc" });
+		            $(this).addClass('filter-selected');
+		            $('#SortCloudByStrength').removeClass('filter-selected');
+		        }
+		    );
+
+    $('#SortCloudByStrength').toggle(
+		        function () {
+		            $("#tags ul li").tsort({ order: "desc", attr: "level" });
+		            $(this).addClass('filter-selected');
+		            $('#sortCloudByAlphabet').removeClass('filter-selected');
+		        },
+		        function () {
+		            $("#tags ul li").tsort({ order: "asc", attr: "level" });
+		            $(this).addClass('filter-selected');
+		            $('#sortCloudByAlphabet').removeClass('filter-selected');
+		        }
+		    );
+
+
+});
